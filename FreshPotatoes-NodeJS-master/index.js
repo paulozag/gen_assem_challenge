1dfa8a18ec5bcc0507542e83e023d193987e3bca
paulozag <ozag@comcast.net>
2017-08-03 13:52:09 -0700

    hits db, but async issues

--- FreshPotatoes-NodeJS-master/index.js
+++ FreshPotatoes-NodeJS-master/index.js
@@ -1,4 +1,4 @@
-const sqlite = require('sqlite'),
+const sqlite = require('sqlite3'),
       Sequelize = require('sequelize'),
       request = require('request'),
       express = require('express'),
@@ -6,21 +6,34 @@ const sqlite = require('sqlite'),
 
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
 
+
+// CREATE DB CONNECTION
+var db = new sqlite.Database('./db/database.db');
+
 // START SERVER
 Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
+
 
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
+  let filmID = req.params.id
+  db.all('select * from films where id=?', filmID, function(err, rows){
+    console.log(rows[0])
+  })
+
+
   responseObject = getResponseObject();
   console.log(responseObject)
   res.setHeader('Content-Type', 'application/json');
   res.status(200);
   res.send(JSON.stringify(responseObject));
+
+
 }
 
 let getResponseObject = () => {
